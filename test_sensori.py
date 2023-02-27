9c4c6f53229c392143dc5df80278b91ad8491a84
itsDamon <nick.parise20@gmail.com>
2023-02-27 20:48:07 +0100

    seriale e sensori

--- test_sensori.py
+++ test_sensori.py
@@ -11,9 +11,6 @@ except:
         sensori = serial.Serial("/dev/ttyACM2", 9600)
 
 # test sensori
-for _ in range(10):
-    print(sensori.readline())
-'''
 print("avanti")
 accendiTofFrontale(sensori)
 sleep(0.3)
@@ -40,4 +37,3 @@ accendiUltrasuoniDestra(sensori)
 sleep(0.3)
 print(sensori.readline())
 spegniSensoreInUso(sensori)
-'''
