f48a6e1913d11ebe1dcf0d389f256926d3023c27
itsDamon <nick.parise20@gmail.com>
2023-03-03 16:09:18 +0100

    Aggiornamento main

--- segui_linea_con_sensori.py
+++ segui_linea_con_sensori.py
@@ -6,7 +6,7 @@ from picamera2 import Picamera2
 
 from variabiliGlobali import *
 
-dim = int(MAXX / 100 * 24)  # dimensione aree nere
+dim = int(MAXX / 100 * 27)  # dimensione aree nere
 
 cv2.startWindowThread()  # permette l'aggiornamento di cv2.imshow()
 camera = Picamera2()  # assegna la videocamera e assegna il video a camera
