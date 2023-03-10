3dc58885293273534d9e1f5cf754615797cb2754
leor0 <lucascudeller05@gmail.com>
2023-03-10 15:46:19 +0100

    Crop fix chuck6 alzo di 10pixel

--- variabiliGlobali.py
+++ variabiliGlobali.py
@@ -5,4 +5,4 @@ offset = 20  # distanza dal bordo
 dim = int(MAXX / 100 * 24)  # dimensione aree nere
 MINY = 310  # lunghezza dal crop basso
 MINY2 = 260  # lunghezza del crop dell'incroci0
-CROPSTART = 420 #start del crop
+CROPSTART = 410 #start del crop
