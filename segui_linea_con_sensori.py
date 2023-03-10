7defd4569715a86be4779182e67e81d5076f7ab0
leor0 <lucascudeller05@gmail.com>
2023-03-10 15:15:17 +0100

    Crop fix chuck2

--- segui_linea_con_sensori.py
+++ segui_linea_con_sensori.py
@@ -10,7 +10,7 @@ cv2.startWindowThread()  # permette l'aggiornamento di cv2.imshow()
 camera = Picamera2()  # assegna la videocamera e assegna il video a camera
 camera.configure(camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (MAXX, MAXY)}))  # configura la videocamera
 camera.controls.Brightness = 0
-camera.set_controls({"ExposureTime": 4000, "AnalogueGain": 1.0, "AeEnable": 0})  # controllo esposizione
+camera.set_controls({"ExposureTime": 3000, "AnalogueGain": 1.0, "AeEnable": 0})  # controllo esposizione
 camera.start()  # avvia la videocamera
 sleep(2)  # pausa 2s
 
--- variabiliGlobali.py
+++ variabiliGlobali.py
@@ -4,4 +4,4 @@ MAXY = 480
 offset = 20  # distanza dal bordo
 dim = int(MAXX / 100 * 24)  # dimensione aree nere
 MINY = 380  # lunghezza dal crop basso
-MINY2 = 320  # lunghezza del crop dell'incrocio
+MINY2 = 320  # lunghezza del crop dell'incroci0
