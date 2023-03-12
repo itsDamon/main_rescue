59ccaeef2fffb4666324699a3de63095b0b8aeff
leor0 <lucascudeller05@gmail.com>
2023-03-12 15:56:07 +0100

    ritorno alle origini

--- variabiliGlobali.py
+++ variabiliGlobali.py
@@ -6,5 +6,5 @@ dim = int(MAXX / 100 * 24)  # dimensione aree nere
 MINY = 310  # lunghezza dal crop basso
 MINY2 = 260  # lunghezza del crop dell'incroci0
 CROPSTART = 410 #start del crop
-areaValidaMax = 500 #area valida per il nero
-areaValidaMin = 200 #area valida per il nero
+areaValidaMax = 1000 #area valida per il nero
+areaValidaMin = 120 #area valida per il nero
