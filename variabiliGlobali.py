b06aafcf5d7aa272fb5e97bdc82cb3f6b788dabb
itsDamon <nick.parise20@gmail.com>
2023-03-13 10:36:29 +0100

    no step

--- variabiliGlobali.py
+++ variabiliGlobali.py
@@ -1,10 +1,10 @@
 STATO = 0
 MAXX = 640
 MAXY = 480
-offset = 55  # distanza dal bordo
-dim = int(MAXX / 100 * 23)  # dimensione aree nere
-MINY = 280  # lunghezza dal crop basso
-MINY2 = 230  # lunghezza del crop dell'incroci0
+offset = 30  # distanza dal bordo
+dim = int(MAXX / 100 * 20)  # dimensione aree nere
+MINY = 310  # lunghezza dal crop basso
+MINY2 = 280  # lunghezza del crop dell'incroci0
 CROPSTART = 410  # start del crop delle aree nere aka 480-CROPSTART DISTANZA BASSA
 areaValidaMax = 1000  # area valida per il nero
 areaValidaMin = 120  # area valida per il nero
